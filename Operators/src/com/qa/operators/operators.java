463226916ec400dcc7f2c7a296ca9c8a19133170
LewisPearsonGitHub <lewiscpearson@gmail.com>
2022-05-24 11:15:06 +0100

    Merged projects

--- Operators/src/com/qa/operators/operators.java
+++ Operators/src/com/qa/operators/operators.java
@@ -1,37 +1,45 @@
 package com.qa.operators;
-
-public class operators {
-
-	public static void main(String[] args) {
-		int int1 = 45;
-		int int2 = 2;
+public class Operators {
+	
+	public 	int int1;
+	public int int2;
+	
+	public double dub1;
+	public double dub2;
+	
+	public static String answer;
+	
+	public Operators() {
 		
-		double dub1 = 45;
-		double dub2 = 2;
-		
-		System.out.println(int1 + int2);
-		operators.multi(int1, int2);
-		operators.sub(int1, int2);
-		operators.division(int1, int2);
-		operators.division1(dub1, dub2);
-	}
-
-	public static void multi(int int1, int int2) {
-		System.out.println(int1 * int2);
 	}
 	
-	public static void sub(int int1, int int2) {
-		System.out.println(int1 - int2);
+	public Operators(int int1, int int2, double dub1, double dub2) {
+		this.int1 = int1;
+		this.int2 = int2;
+		this.dub1= dub1;
+		this.dub2 = dub2;
+		}
+	
+	
+	
+	public int multi(int int1, int int2) {
+		return int1 * int2;
 	}
-	public static void division(int int1, int int2) {
-		System.out.println(int1 / int2);
+	
+	public int sub(int int1, int int2) {
+		return int1 - int2;
 	}
-	public static void division1(double dub1, double dub2) {
+	public int division(int int1, int int2) {
+		return int1 / int2;
+	}
+	public static Double division1(double dub1, double dub2) {
 	if (dub1 < dub2) {
-	System.out.println(dub1 / dub2);
+	double answer = (dub1 / dub2);
+	return answer;
 	} else {
 		System.out.println("This division cannot be performed.");
 	}
+	return 0.0;
 	}
 	
 	
